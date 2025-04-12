03ca4902026bf2e90d5a9caa58ba3b176d1c6a13
ralcadev09 <121913575+ralcadev09@users.noreply.github.com>
2025-04-12 03:09:45 +0000

    Proyecto Excuse Generator Final

--- src/app.js
+++ src/app.js
@@ -10,18 +10,15 @@ let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * (max));
 }
 
 
 function getRandom(anyArray) {
   let max = anyArray.length - 1;
-  let min = 0;
-  let random = getRandomNumber(min, max)
+  let random = getRandomNumber(max)
   return anyArray[random]
-
-
 }
 
 window.onload = function () {
